90083cb07dc5d6a3c23e5cb543439330e5a5c0e0
Javier <javiepc@gmail.com>
2020-03-18 10:23:24 -0300

    Primer commit

---
+++ WebScrapingDumy/src/main/java/com/web/scraping/WebScrapingDumyApplication.java
@@ -0,0 +1,13 @@
+package com.web.scraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrapingDumyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrapingDumyApplication.class, args);
+	}
+
+}
---
+++ WebScrapingDumy/src/test/java/com/web/scraping/WebScrapingDumyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.scraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapingDumyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
