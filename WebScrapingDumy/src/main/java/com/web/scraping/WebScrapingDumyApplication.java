88b3822484f8460ee9234974bf85ff88855523fb
Javier <javiepc@gmail.com>
2020-03-18 10:32:22 -0300

    Primer commit

--- WebScrapingDumy/src/main/java/com/web/scraping/WebScrapingDumyApplication.java
+++ WebScrapingDumy/src/main/java/com/web/scraping/WebScrapingDumyApplication.java
@@ -3,6 +3,7 @@ package com.web.scraping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Rama de desarrollo
 @SpringBootApplication
 public class WebScrapingDumyApplication {
 
